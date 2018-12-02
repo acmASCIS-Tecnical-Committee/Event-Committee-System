d814f66b236f30276c362bc80d49295bf3a368ac
mahmoudali <mahmoud.ali.fcis@gmail.com>
2018-12-02 03:11:33 +0200

    created all the schemas for the different databases we will have, also added header for each to describe it

---
+++ models/owner.js
@@ -0,0 +1,8 @@
+// Def: Owner is a crew/non crew person who owns 0 or more resources
+// Data details can be found on a trello card named the same name as this file
+// this is a schema for how the owner instance will look like in the DB
+// you will need mongoose 'mongoDB' to import the schema module
+const mongoose = require("mongoose");
+
+// use mongo schema as Schema
+const Schema = mongoose.Schema;
---
+++ models/space.js
@@ -0,0 +1,8 @@
+// Def: Space is how we will represent co-working space
+// Data details can be found on a trello card named the same name as this file
+// this is a schema for how the co-working space instance will look like in the DB
+// you will need mongoose 'mongoDB' to import the schema module
+const mongoose = require("mongoose");
+
+// use mongo schema as Schema
+const Schema = mongoose.Schema;
