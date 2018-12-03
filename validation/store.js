bebdf05974e7f451696adc1641129a29ee2fee95
mahmoudali <mahmoud.ali.fcis@gmail.com>
2018-12-03 03:19:16 +0200

    added comments and return errors to illustrate how the errors object works

--- validation/store.js
+++ validation/store.js
@@ -3,6 +3,15 @@ const validator = require("validator");
 // now you can use isEmpty(anything) to check if it's empty or not
 const isEmpty = require("./is_empty");
 
+// data is the data submitted by the form, you will find the name of the attributes in the trello card
+// you should return every error you got in the errors.field = "message of the error"
+// for example errors.name = "Name must have between 6 and 100 characters"
 module.exports = function validateStoreInput(data) {
   // your code goes here
+  let errors = {};
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
 };
