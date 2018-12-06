610ef396f4a8eb20ec3e60769cddc4e7da5bf852
mahmoudali <mahmoud.ali.fcis@gmail.com>
2018-12-06 20:28:51 +0200

    validator was written twice

--- validation/login.js
+++ validation/login.js
@@ -7,35 +7,16 @@ const isEmpty = require("./is_empty");
 // you should return every error you got in the errors.field = "message of the error"
 // for example errors.name = "Name must have between 6 and 100 characters"
 
-
-/*
- DESC: 
-The login page.
-
-Data will contain:
-
-1. email
-2. password
-
-validations:
-1. Required, Not empty, valid email.
-2. Required, Not empty.
-
-to code use the .js file in the path written in the name of the card.
- */
-const validator = require("validator");
-const isEmpty = require("./is_empty");
-
 module.exports = function validateLoginInput(data) {
   // your code goes here
   let errors = {};
 
-  if(isEmpty(data.email)||!validator.isEmail(data.email)){
-    errors.email='You must write a valid email'
+  if (isEmpty(data.email) || !validator.isEmail(data.email)) {
+    errors.email = "You must write a valid email";
   }
 
-  if(isEmpty(data.password)){
-    errors.password='You must provied a password'
+  if (isEmpty(data.password)) {
+    errors.password = "You must provied a password";
   }
 
   return {
