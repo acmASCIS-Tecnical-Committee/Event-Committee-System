c41943f691494e9110e7d5a2f46971a735a30a33
mahmoudali <mahmoud.ali.fcis@gmail.com>
2018-12-10 01:57:11 +0200

    final touches + comments

--- validation/login.js
+++ validation/login.js
@@ -1,14 +1,20 @@
-// to use some built in validators
+// @Def: Function to check if the login data is right
+// @Name: use validateLoginInput()
+// @Params:
+// 1. Data submitted from the login form
+// Req: (email, password)
+// @returns
+// 1. errors: object contain error in each field, example : email= not a valid email
+// 2. isValid: True if it found no errors, False otherwise
+
+// Expected bugs: Maybe you would need to assign "" to any row of data that isEmpty() = true
+
+// for some built in validator functions
 const validator = require("validator");
-// now you can use isEmpty(anything) to check if it's empty or not
+// now you can use isEmpty(anything) to check if it's empty or not since we don't trust validator.isEmpty
 const isEmpty = require("./is_empty");
 
-// data is the data submitted by the form, you will find the name of the attributes in the trello card
-// you should return every error you got in the errors.field = "message of the error"
-// for example errors.name = "Name must have between 6 and 100 characters"
-
 module.exports = function validateLoginInput(data) {
-  // your code goes here
   let errors = {};
 
   if (isEmpty(data.email) || !validator.isEmail(data.email)) {
