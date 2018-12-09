e869f649ad4cb0f1797c2aa2dd7dcbca829d906e
mahmoudali <mahmoud.ali.fcis@gmail.com>
2018-12-10 01:35:26 +0200

    changed the name + comments + final touches

--- models/store.js
+++ models/store.js
@@ -1,13 +1,13 @@
-// Def: Store is a place where we can buy one or more material
-// Data details can be found on a trello card named the same name as this file
-// this is a schema for how the store instance will look like in the DB
-// you will need mongoose 'mongoDB' to import the schema module
+// @Def: Store is a place where we can buy one or more material
+// @Name: stores
+// @Relation: many to many relation with materials, so materials refrence here
+// --> many stores can provide the same material, many materials could be provided by the same store
 const mongoose = require("mongoose");
 
 // use mongo schema as Schema
 const Schema = mongoose.Schema;
 
-const StoreSchema = new Schema({
+const storeSchema = new Schema({
   address: {
     Link: {
       type: String,
@@ -51,4 +51,4 @@ const StoreSchema = new Schema({
   }
 });
 
-module.exports = User = mongoose.model("store", StoreSchema);
+module.exports = User = mongoose.model("stores", storeSchema);
