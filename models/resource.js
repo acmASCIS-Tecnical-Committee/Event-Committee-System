7b1383519d697c5fe530eb986090b1f36304736f
mahmoudali <mahmoud.ali.fcis@gmail.com>
2018-12-10 01:30:43 +0200

    finished the task + final touches + comments

--- models/resource.js
+++ models/resource.js
@@ -1,8 +1,30 @@
-// Def: Resource is something that acmASCIS owns that's left with someone (Owner)
-// Data details can be found on a trello card named the same name as this file
-// this is a schema for how the resource instance will look like in the DB
-// you will need mongoose 'mongoDB' to import the schema module
+// @Def: Resource is something that acmASCIS owns that's left with someone (Owner)
+// @Name: resources
+// @Relation: each resource have one owner
 const mongoose = require("mongoose");
 
 // use mongo schema as Schema
 const Schema = mongoose.Schema;
+
+const resourceSchema = new Schema({
+  name: {
+    // should be unique, in case of duplications concatenate an id to the name
+    type: String,
+    required: true
+  },
+  updated: {
+    type: Date,
+    default: Date.now
+  },
+  details: {
+    type: String,
+    required: false
+  },
+  owner: {
+    // ID refrences to "owners database"
+    type: Schema.Types.ObjectId,
+    ref: "owners"
+  }
+});
+
+module.exports = resource = mongoose.model("resources", resourceSchema);
