6035ca48deba53220d43b8a5bdf324effb48afaa
mahmoudali <mahmoud.ali.fcis@gmail.com>
2018-12-10 02:24:39 +0200

    finished the task + comment + final touches

--- validation/resource.js
+++ validation/resource.js
@@ -1,15 +1,43 @@
+// @Def: Function to check if the resource data is sufficient
+// @Name: use validateResourceInput() after including
+// @Params:
+// 1. Data submitted from the add new resource form
+// Req: (name, details, owner) -- updated is set automatically to date.now
+// @returns
+// 1. errors: object contain error in each field, example : email= not a valid email
+// 2. isValid: True if it found no errors, False otherwise
+
 // to use some built in validators
 const validator = require("validator");
 // now you can use isEmpty(anything) to check if it's empty or not
 const isEmpty = require("./is_empty");
 
-// data is the data submitted by the form, you will find the name of the attributes in the trello card
-// you should return every error you got in the errors.field = "message of the error"
-// for example errors.name = "Name must have between 6 and 100 characters"
 module.exports = function validateResourceInput(data) {
-  // your code goes here
   let errors = {};
 
+  // Fixed name criteria
+  let name_criteria = { min: 6, max: 100 };
+
+  // Fixed notes criteria
+  let notes_critera = { min: 0, max: 500 };
+
+  // name: Required, Not empty, between 6 and 100.
+  if (isEmpty(data.name) || !validator.isLength(data.name, name_criteria)) {
+    errors.name = "Name length should be between 6 and 10 characters";
+  }
+
+  // details: Required, Not empty, between 10 and 500
+  if (
+    isEmpty(data.details) ||
+    !validator.isLength(data.details, notes_critera)
+  ) {
+    errors.name = "Detials should be between 10 and 500 characters";
+  }
+
+  // owner: Required.
+  if (isEmpty(data.owner)) {
+    errors.owner = "You must provide an owner";
+  }
   return {
     errors,
     isValid: isEmpty(errors)
