bfaf8889e28960200de474bd3c4ad5d3bc03c8e7
mahmoudali <mahmoud.ali.fcis@gmail.com>
2019-02-07 01:51:44 +0200

    added passport configuration

---
+++ config/passport.js
@@ -0,0 +1,25 @@
+const JWTstrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
+const keys = require("./keys");
+
+const opts = {};
+// the strategy of extraction
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+// the secret we used to make the token in the first place
+opts.secretOrKey = keys.secret;
+
+module.exports = passport => {
+  passport.use(
+    new JWTstrategy(opts, (jwtPayload, done) => {
+      //jwtpayload is the token after being decrypted using the secret
+      User.findById(jwtPayload.id)
+        .then(user => {
+          // in case of successful authentication, the done call back should give a good session, otherwise unotherized
+          return done(null, user);
+        })
+        .catch(err => console.log(err));
+    })
+  );
+};
