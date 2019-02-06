7654d192fd930b885ebf3e0a1143eda20f17bf1b
mahmoudali <mahmoud.ali.fcis@gmail.com>
2019-02-07 01:52:55 +0200

    added the needed lines for configuring passport

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express"); // include express so we can use it -> app
 const mongoose = require("mongoose"); // to connect to the db
 const bodyParser = require("body-parser"); // use bodyparser to parse req.body
+const passport = require("passport"); // use passport to validate JWT
 
 const store = require("./routes/api/store");
 const user = require("./routes/api/user");
@@ -20,10 +21,17 @@ mongoose
   .then(() => console.log("Database Connected."))
   .catch(err => console.log(err));
 
-// use routes
+// Passport middleware - to make express use passport as middleware, appearently it's not necessary now
+// this part i don't understand well (mahmoud3ali)
+app.use(passport.initialize());
 
+// Passport config, call the passport.js and fix it's setting make it ready to be used by authentication
+require("./config/passport")(passport);
+
+// use routes
 // route to test store, maybe we will remove it in future
 app.use("/api/store", store);
+// route to user login/register
 app.use("/api/user", user);
 
 const port = 5000; // use localhost:5000
