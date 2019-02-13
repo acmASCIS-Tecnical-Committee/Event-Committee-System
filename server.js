4deee70194c900c8a11fe1c4e73f4ea709fb3daa
mahmoudali <mahmoud.ali.fcis@gmail.com>
2019-02-13 20:03:58 +0200

    added material route and commented some lines

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require("express"); // include express so we can use it -> app
 const mongoose = require("mongoose"); // to connect to the db
 const bodyParser = require("body-parser"); // use bodyparser to parse req.body
 const passport = require("passport"); // use passport to validate JWT
+const settings = require("./config/settings"); // to import ports from settings
 
+// Import APIs
 const store = require("./routes/api/store");
 const user = require("./routes/api/user");
+const material = require("./routes/api/material");
 
 const app = express();
 
@@ -28,12 +31,15 @@ app.use(passport.initialize());
 // Passport config, call the passport.js and fix it's setting make it ready to be used by authentication
 require("./config/passport")(passport);
 
-// use routes
-// route to test store, maybe we will remove it in future
+// API routes
+// route to store API
 app.use("/api/store", store);
-// route to user login/register
+// route to user API
 app.use("/api/user", user);
+// route to material API
+app.use("/api/material", material);
 
-const port = 5000; // use localhost:5000
-
-app.listen(port, () => console.log("server running " + port));
+// use localhost:5000
+app.listen(settings.backendPort, () =>
+  console.log("server running " + settings.backendPort)
+);
